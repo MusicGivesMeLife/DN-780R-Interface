6ff3d865037a8bf1f497103c1a03cf3dd2fee777
Thomas McInnes <tommcinnes1@outlook.com>
2020-03-09 11:35:35 -0400

    Added more functions, updated README
    rec, pause_rec, forward, rewind, and direction

--- README.md
+++ README.md
@@ -4,18 +4,33 @@
 
 ## Functions
 ### `__init__`
-Accepts a serial port as a string and opens the connection with a baud of 9600, even parity, and runs `status` and `tape_stat`.
+Accepts a serial port as a string and opens the connection with a baud of 9600, even parity, and runs `all_stat`.
 
 ### `reset`
 Performs a soft reset on the recorder.
 
+### `all_stat`
+Runs `status`, `tape_stat`, and `established`
+
 ### `status`
 Gets current system status from recorder.
-* `a_loaded` 'True' if a tape is loaded in mecha A, 'False' if not
-* `b_loaded` 'True' if a tape is loaded in mecha B, 'False' if not
-* `highspeed` 'True' if set to high-speed mode, 'False' if not
-* `counter_a` Current counter value for mecha A
-* `counter_b` Current counter value for mecha B
+* `a_stat` Byte.
+* `b_stat` Byte.
+  * 'A' No tape
+  * 'B' Stop
+  * 'C' Play
+  * 'D' Record Pause
+  * 'E' Record
+  * 'F' Record Mute
+  * 'G' Fast Forward
+  * 'H' Rewind
+  * 'I' Cue
+  * 'J' Review
+  * 'K' Play Mute
+* `highspeed` Boolean. 'True' if set to high-speed mode, 'False' if not
+* `counter_a` Integer. Current counter value for mecha A
+* `counter_b` Integer. Current counter value for mecha B
+* `sys_status` Integer.
 
 ### `cpu_vers`
 Returns CPU version in the format of a 4-digit string.
@@ -24,14 +39,22 @@ Returns CPU version in the format of a 4-digit string.
 Gets current recordability of tapes in both mechas.
 * `a_loaded` Same as above
 * `b_loaded` Same as above
-* `reca_a` True is mecha A side A is recordable
-* `reca_b` True is mecha A side B is recordable
-* `recb_a` True is mecha B side A is recordable
-* `recb_a` True is mecha B side B is recordable
+* `reca_a` Boolean. True is mecha A side A is recordable
+* `reca_b` Boolean. True is mecha A side B is recordable
+* `recb_a` Boolean. True is mecha B side A is recordable
+* `recb_a` Boolean. True is mecha B side B is recordable
 
 ### `established`
 Return hardware settings.
-TODO
+* `dup` Integer. Duplication status, 0 if off, 1 if slave, and 2 if master
+* `rev` Integer. Reverse status, 0 if single, 1 if loop, 2 if relay, 3 if cascade
+* `dolbya` Integer. Mecha A Dolby Noise Reduction. 0 if off, 1 if type B, 2 if type C
+* `dira` Boolean. Mecha A play/rec direction. True if forward, False if reverse
+* `mema` Boolean. Mecha A memory. True if on, False if off
+* `dolbyb` Integer. Mecha A Dolby Noise Reduction. 0 if off, 1 if type B, 2 if type C
+* `dirb` Boolean. Mecha A play/rec direction. True if forward (right), False if reverse (left)
+* `memb` Boolean. Mecha A memory. True if on, False if off
+*Note, direction might be different from panel switch if changed via command*
 
 ### `id`
 Returns recorder model as a string.
@@ -41,6 +64,23 @@ Accepts a string indicating which mecha, 'A' or 'B', and sets that mecha to play
 
 ### `stop`
 Accepts a string indicating which mecha, 'A' or 'B', and sets that mecha to stop.
+
+### `rec`
+Accepts a string indicating which mecha, 'A' or 'B', and sets that mecha to record.
+* Sets to recording standby if mecha status is stop
+* Makes a 5sec muted recording if mecha status is record
+
+### `pause_rec`
+Accepts a string indicating which mecha, 'A' or 'B', and sets that mecha to paused record.
+
+### `forward`
+Accepts a string indicating which mecha, 'A' or 'B', and a boolean indicating if music search is to be used and sets the mecha to fast-forward.
+
+### `rewind`
+Accepts a string indicating which mecha, 'A' or 'B', and a boolean indicating if music search is to be used and sets the mecha to rewind.
+
+### `direction`
+Accepts a string indicating which mecha, 'A' or 'B', and toggles the direction of that mecha.
 
 ### `close`
 Ends serial connection to recorder.
--- dn780r.py
+++ dn780r.py
@@ -4,9 +4,13 @@ import time
 class Deck:
     def __init__(self, port):
         self.port = port
-        self.s = serial.Serial(str(self.port), 9600, parity=serial.PARITY_EVEN, timeout=1)
+        self.s = serial.Serial(str(self.port), 9600, parity=serial.PARITY_EVEN, timeout=5)
+        all_stat()
+        return 0
+    def all_stat():
         self.status()
         self.tape_stat()
+        self.established()
         return 0
     def reset(self):
         self.s.flushInput()
@@ -17,15 +21,9 @@ class Deck:
         self.s.flushInput()
         self.s.write(b'\x02\x30\x00\x00\x00\x00\x03\x33\x33')
         self.ret = list(str(self.s.read(20), 'UTF-8'))
-        self.sys_status = int(self.ret[3])
-        if self.ret[5] == 'A':
-            self.a_loaded = False
-        elif self.ret[5] == 'B':
-            self.a_loaded = True
-        if self.ret[5] == 'A':
-            self.b_loaded = False
-        elif self.ret[11] == 'B':
-            self.b_loaded = True
+        self.sys_status = str(self.ret[3])
+        self.a_stat = self.ret[5]
+        self.b_stat = self.ret[11]
         if self.ret[4] == '0':
             self.highspeed = False
         elif self.ret[4] == '1':
@@ -48,50 +46,50 @@ class Deck:
         self.s.write(b'\x02\x32\x00\x00\x00\x00\x03\x33\x35')
         self.ret = list(str(self.s.read(8), 'UTF-8'))
         if self.ret[4] == '0':
-            self.a_loaded = False
+            self.a_stat = 'A'
             self.recb_a == False
             self.recb_b == False
         elif self.ret[4] == '1':
-            self.a_loaded = True
             self.reca_a == True
             self.reca_b == True
         elif self.ret[4] == '2':
-            self.a_loaded = True
             self.reca_a == False
             self.reca_b == True
         elif self.ret[4] == '3':
-            self.a_loaded = True
             self.reca_a == True
             self.reca_b == False
-        elif self.ret[4] == '1':
-            self.b_loaded = True
+        elif self.ret[4] == '4':
             self.recb_a == False
             self.recb_b == False
         if self.ret[5] == '0':
-            self.b_loaded = False
+            self.b_stat = 'A'
             self.recb_a == False
             self.recb_b == False
         elif self.ret[5] == '1':
-            self.b_loaded = True
             self.recb_a == True
             self.recb_b == True
         elif self.ret[5] == '2':
-            self.b_loaded = True
             self.recb_a == False
             self.recb_b == True
         elif self.ret[5] == '3':
-            self.b_loaded = True
             self.recb_a == True
             self.recb_b == False
-        elif self.ret[5] == '1':
-            self.b_loaded = True
+        elif self.ret[5] == '4':
             self.recb_a == False
             self.recb_b == False
         return 0
     def established(self):
         self.s.flushInput()
         self.s.write(b'\x02\x33\x00\x00\x00\x00\x03\x33\x36')
-        self.ret = str(self.s.readline(), 'UTF-8')
+        self.ret = list(str(self.s.read(14), 'UTF-8'))
+        self.dup = int(self.ret[3])
+        self.rev = int(self.ret[4])
+        self.dolbya = int(self.ret[5])
+        self.dira = not(bool(int(self.ret[6])))
+        self.mema = bool(int(self.ret[7]))
+        self.dolbyb = int(self.ret[8])
+        self.dirb = not(bool(int(self.ret[9])))
+        self.memb = bool(int(self.ret[10]))
     def id(self):
         self.s.flushInput()
         self.s.write(b'\x02\x34\x00\x00\x00\x00\x03\x33\x37')
@@ -104,14 +102,61 @@ class Deck:
             self.s.write(b'\x02\x40\x30\x00\x00\x00\x03\x37\x33')
         elif mecha == 'B':
             self.s.write(b'\x02\x40\x31\x00\x00\x00\x03\x37\x34')
-        self.ret = str(self.s.readline(), 'UTF-8')
+        self.ret = str(self.s.read(6), 'UTF-8')
     def stop(self, mecha):
         self.s.flushInput()
         if mecha == 'A':
             self.s.write(b'\x02\x41\x30\x00\x00\x00\x03\x37\x34')
         elif mecha == 'B':
             self.s.write(b'\x02\x41\x31\x00\x00\x00\x03\x37\x35')
-        self.ret = str(self.s.readline(), 'UTF-8')
+        self.ret = str(self.s.read(6), 'UTF-8')
+    def rec(self, mecha):
+        self.s.flushInput()
+        if mecha == 'A':
+            self.s.write(b'\x02\x42\x30\x00\x00\x00\x03\x37\x35')
+        elif mecha == 'B':
+            self.s.write(b'\x02\x42\x31\x00\x00\x00\x03\x37\x36')
+        self.ret = str(self.s.read(6), 'UTF-8')
+    def pause_rec(self, mecha):
+        self.s.flushInput()
+        if mecha == 'A':
+            self.s.write(b'\x02\x43\x30\x00\x00\x00\x03\x37\x36')
+        elif mecha == 'B':
+            self.s.write(b'\x02\x43\x31\x00\x00\x00\x03\x37\x37')
+        self.ret = str(self.s.read(6), 'UTF-8')
+    def forward(self, mecha, msearch):
+        self.s.flushInput()
+        if mecha == 'A':
+            if msearch == True:
+                self.s.write(b'\x02\x44\x30\x31\x00\x00\x03\x41\x38')
+            else:
+                self.s.write(b'\x02\x44\x30\x30\x00\x00\x03\x41\x37')
+        elif mecha == 'B':
+            if msearch == True:
+                self.s.write(b'\x02\x44\x31\x31\x00\x00\x03\x41\x39')
+            else:
+                self.s.write(b'\x02\x44\x31\x30\x00\x00\x03\x41\x38')
+        self.ret = str(self.s.read(6), 'UTF-8')
+    def rewind(self, mecha, msearch):
+        self.s.flushInput()
+        if mecha == 'A':
+            if msearch == True:
+                self.s.write(b'\x02\x45\x30\x31\x00\x00\x03\x41\x39') #TODO
+            else:
+                self.s.write(b'\x02\x45\x30\x30\x00\x00\x03\x41\x38')
+        elif mecha == 'B':
+            if msearch == True:
+                self.s.write(b'\x02\x45\x31\x31\x00\x00\x03\x41\x40') #TODO
+            else:
+                self.s.write(b'\x02\x45\x31\x30\x00\x00\x03\x41\x39')
+        self.ret = str(self.s.read(6), 'UTF-8')
+    def direction(self, mecha):
+        self.s.flushInput()
+        if mecha == 'A':
+            self.s.write(b'\x02\x46\x30\x00\x00\x00\x03\x37\x39')
+        elif mecha == 'B':
+            self.s.write(b'\x02\x46\x31\x00\x00\x00\x03\x37\x41')
+        self.ret = str(self.s.read(6), 'UTF-8')
     def close(self):
         self.s.close()
         return 0
